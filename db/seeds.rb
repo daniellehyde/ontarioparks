2803282f2b036357377b5d7d40a3663626604aa0
Colton Pauderis <colton.pauderis@gmail.com>
2021-07-31 17:09:32 -0400

    More seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-park_data = JSON.load (File.new('db/small_parks.json'))
+Park.delete_all
+park_data = JSON.load (File.new('db/all_parks.json'))
 park_data.each do |park|
     Park.new(external_park_id: park['park_id'], name: park['full_name'], city: park['address']['city']).save!
 end
 
-campsite_data = JSON.load (File.new('db/small_sites.json'))
+Campsite.delete_all
+campsite_data = JSON.load (File.new('db/all_sites.json'))
 campsite_data.each do |campsite|
     park = Park.where(external_park_id: campsite['park_id']).first
     Campsite.new(name: campsite['name'], quality: campsite['quality'], privacy: campsite['privacy'], park: park).save!
